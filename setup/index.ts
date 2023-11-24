a3e15c39f72d30a2198312cd7eab2c39678c1cc0
louiss0 <louisshelton0gmail.com>
2023-11-24 07:06:15 -0500

    feat: Add setup files to the vitest repo

---
+++ setup/index.ts
@@ -0,0 +1,6 @@
+import { expect } from 'vitest';
+
+
+expect.extend({
+
+})
---
+++ vitest.d.ts
@@ -0,0 +1,8 @@
+import type { Assertion, AsymmetricMatchersContaining } from "vitest"
+interface CustomMatchers<R = unknown> {
+}
+
+declare module 'vitest' {
+    interface Assertion<T = any> extends CustomMatchers<T> { }
+    interface AsymmetricMatchersContaining extends CustomMatchers { }
+}
